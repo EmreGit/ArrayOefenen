3416e53b81888e4a3ccfaed63f0ec127ae899f13
Emre Elagoz <emreelagoz1994@gmail.com>
2020-10-20 22:42:50 +0200

    Add project files.

---
+++ ArrayOefenen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOefenen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userinput = new string [10];
+            Array.Sort(userinput);
+
+            for (int i = 0; i < userinput.Length; i++)
+            {
+                userinput[i] = Console.ReadLine();
+                
+            }
+            foreach (string item in userinput)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ IfStatements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int userInput = int.Parse(Console.ReadLine());
+            
+
+            if ((userInput % 400) == 0)
+            {
+                Console.WriteLine("{0} is a leap year", userInput);
+            }
+
+            if ((userInput % 100) == 0)
+            {
+                Console.WriteLine("{0} is not a leap year", userInput);
+            }
+
+            if ((userInput % 4) == 0)
+            {
+                Console.WriteLine("{0} is a leap year", userInput);
+            }
+
+            else
+            {
+                Console.WriteLine("{0} is not a leap year", userInput);
+            }
+
+        }
+    }
+}
